04b7c3d030e0d9584106ff32845f8ac40bb49fe5
tmcode7 <mawuenatord7@gmail.com>
2022-01-19 22:36:55 +0000

    added javascript to listen to submit event and update list

---
+++ script.js
@@ -0,0 +1,27 @@
+const addItems = document.querySelector('.add-Items')
+const itemsList = document.querySelector('.plates')
+const items = []
+
+function addItem(e) {
+    e.preventDefault()
+    const text = (this.querySelector('[name=item]')).value   //using the'this' keyword specifies the form to be selected
+    const item = {
+        text,
+        done: false
+    }
+    items.push(item)
+    populateList(item, itemsList)
+    this.reset()  //clears the input area
+}
+
+function populateList(plates=[], placeList) {
+    platesList.innerHTML = plates.map((plate, i) => {
+        return `
+        <li>
+        <label for="">${plate.text}</label>
+        </li>
+        ` 
+    }).join('')
+}
+
+addItems.addEventListener('submit', addItem)
